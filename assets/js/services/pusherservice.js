59e57c5eaf269355c5a47ac56304f9e1695dd000
David Wood <david.p.wood@gmail.com>
2016-03-19 22:26:18 +0000

    Use new auth server

--- assets/js/services/pusherservice.js
+++ assets/js/services/pusherservice.js
@@ -1,7 +1,7 @@
 goldfish.factory('$pusherService', ($pusher) => {
   const client = new Pusher('e59022bc804f00e0add7', {
     cluster: 'eu',
-    authEndpoint: 'http://127.0.0.1:5000/pusher/auth',
+    authEndpoint: 'https://goldfish-auth.herokuapp.com/pusher/auth',
     encrypted: true
   });
   const pusher = $pusher(client);
