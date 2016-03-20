94fd7228e9c2d3eac97e44f7c6deed2dbdb3d391
David Wood <david.p.wood@gmail.com>
2016-03-20 03:50:22 +0000

    Code cleanup

--- assets/js/controllers/friends.js
+++ assets/js/controllers/friends.js
@@ -1,10 +1,9 @@
-goldfish.controller('FriendsCtrl', ($scope, $pouchService) => {
-  
+goldfish.controller('FriendsCtrl', ($scope, $pouchService, $log) => {
   $scope.friends = [];
   
   $pouchService.getFriends().then((friends) => {
     $scope.friends = [...$scope.friends, ...friends];
-  });
+  }).catch($log.error);
   
   // $scope.friends = [{
   //   name: 'Gary Twitter',
