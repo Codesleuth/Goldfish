430fd008b2abb067646b824f709f720450fce248
David Wood <david.p.wood@gmail.com>
2016-03-20 08:44:43 +0000

    Some feed cleanup

--- assets/js/controllers/feed.js
+++ assets/js/controllers/feed.js
@@ -1,10 +1,10 @@
-goldfish.controller('FeedCtrl', ($scope, $log, $pusherService, pouchDB) => {
+goldfish.controller('FeedCtrl', ($scope, $log, $pusherService, $pouchService) => {
   
   $scope.timeago = (timestamp) => moment(timestamp).fromNow();
   
   const pusher = $pusherService.client;
   
-  function bindFriends(friends) {
+  $pouchService.getFriends().then((friends) => {
     friends.forEach((friend) => {
       $log.info('Subscribing to channel private-' + friend.id);
       
@@ -23,43 +23,8 @@ goldfish.controller('FeedCtrl', ($scope, $log, $pusherService, pouchDB) => {
           });
         });
       });
-      
-      
-      
     });
-  }
-  
-  var db = pouchDB('goldfish');
-  
-  db.get('profile')
-    // .catch((err) => {
-    //   if (err.status !== 404) return $log.error(err);
-    //   $log.info('Adding dummy profile...');
-    //   const profile = {
-    //     id: 1,
-    //     name: 'Bobly Bobson'
-    //   };
-    //   db.post('profile', profile);
-    // });
-  
-  db.get('friends').then(bindFriends)
-    // .catch((err) => {
-    //   if (err.status !== 404) return $log.error(err);
-    //   $log.info('Adding dummy friends...');
-    //   const friends = [{
-    //     id: 1,
-    //     name: 'Bobly Bobson'
-    //   },{
-    //     id: 2,
-    //     name: 'Freddy Fredson'
-    //   }];
-    //   db.post('friends', friends);
-    //   bindFriends(friends);
-    // });
-  
-  db.info().then((info) => {
-    $log.info(info);
-  });
+  }).catch($log.error);
 
   $scope.statuses = []
   
